60147ca6bf628950e55892810a18b5df7fde746f
Colin Chauche <colin.chauche@gmail.com>
2021-12-01 10:43:28 -0500

    Complete freelancer rates prompt

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,31 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    8.0 * hourly_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount - before_discount * (discount / 100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    hourly_rate
+    |> daily_rate()
+    |> base_monthly_rate()
+    |> apply_discount(discount)
+    |> Float.ceil()
+    |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    discounted_day_rate =
+      hourly_rate
+      |> daily_rate()
+      |> apply_discount(discount)
+
+    Float.floor(budget / discounted_day_rate, 1)
+  end
+
+  defp base_monthly_rate(daily_rate) do
+    22.0 * daily_rate
   end
 end
