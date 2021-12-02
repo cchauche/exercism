1bcae17708ec096ba2ad7ddc17fc67753a149c41
Colin Chauche <colin.chauche@gmail.com>
2021-12-01 20:19:32 -0500

    Complete darts prompt

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,20 @@ defmodule Darts do
   """
   @spec score(position :: position) :: integer
   def score({x, y}) do
+    distance = :math.sqrt(x * x + y * y)
+
+    cond do
+      distance <= 1 ->
+        10
+
+      distance <= 5 ->
+        5
+
+      distance <= 10 ->
+        1
+
+      true ->
+        0
+    end
   end
 end
