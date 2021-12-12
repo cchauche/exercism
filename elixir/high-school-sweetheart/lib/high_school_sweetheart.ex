9c0f5ca06fcc8128bd9fac69b05e7ce39e63928f
Colin Chauche <colin.chauche@gmail.com>
2021-12-11 23:04:11 -0500

    Complete high school sweatheart prompt

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,31 +1,45 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    {ltr, _} = name |> String.trim() |> String.next_grapheme()
+    ltr
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    init =
+      name
+      |> first_letter()
+      |> String.upcase()
+
+    init <> "."
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    full_name
+    |> String.split(" ")
+    |> Enum.map(&HighSchoolSweetheart.initial/1)
+    |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
+    init1 = initials(full_name1)
+    init2 = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{init1}  +  #{init2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
 
     # Please implement the pair/2 function
   end
