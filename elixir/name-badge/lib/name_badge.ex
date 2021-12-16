bf5140a31eec57db886fa4acb45b16d90a24e81c
Colin Chauche <colin.chauche@gmail.com>
2021-12-15 22:01:25 -0500

    Complete name-badge prompt

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,8 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    # Please implement the print/3 function
+    id = if id, do: "[#{id}] - ", else: ""
+    department = if department, do: department, else: "OWNER"
+
+    id <> "#{name} - #{String.upcase(department)}"
   end
 end
