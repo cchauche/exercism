0dadb79cc732634b901cfde14ec326345e9552e0
Colin Chauche <colin.chauche@gmail.com>
2021-12-16 10:22:12 -0500

    Solve square root problem with recursion

--- elixir/square-root/lib/square_root.ex
+++ elixir/square-root/lib/square_root.ex
@@ -3,6 +3,19 @@ defmodule SquareRoot do
   Calculate the integer square root of a positive integer
   """
   @spec calculate(radicand :: pos_integer) :: pos_integer
+  def calculate(1), do: 1
+
   def calculate(radicand) do
+    find_sqrt(div(radicand, 2), radicand)
+  end
+
+  defp find_sqrt(num, radicand) do
+    cond do
+      num * num == radicand ->
+        num
+
+      true ->
+        find_sqrt(num - 1, radicand)
+    end
   end
 end
