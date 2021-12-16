0441552f636fb49f80cb74cbed96c2de37e422c8
Colin Chauche <colin.chauche@gmail.com>
2021-12-15 22:31:02 -0500

    Solve german-sysadmin prompt

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,31 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  def sanitize([]), do: []
 
-    # Please implement the sanitize/1 function
+  def sanitize([char | tail]) do
+    # ä becomes ae 228
+    # ö becomes oe 246
+    # ü becomes ue 252
+    # ß becomes ss 223
+
+    case char do
+      228 ->
+        'ae'
+
+      246 ->
+        'oe'
+
+      252 ->
+        'ue'
+
+      223 ->
+        'ss'
+
+      char when (char > 96 and char < 123) or char == 95 ->
+        [char]
+
+      _ ->
+        ''
+    end
+    |> Enum.concat(sanitize(tail))
   end
 end
