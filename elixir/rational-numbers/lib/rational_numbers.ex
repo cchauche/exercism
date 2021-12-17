63ead569e78cfb59989de967eab7be2f7587fd01
Colin Chauche <colin.chauche@gmail.com>
2021-12-17 11:40:04 -0500

    Solve through division in rational numbers

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -1,38 +1,49 @@
 defmodule RationalNumbers do
   @type rational :: {integer, integer}
+  import Kernel, except: [abs: 1]
 
   @doc """
   Add two rational numbers
   """
   @spec add(a :: rational, b :: rational) :: rational
-  def add({a1, b1}, {a2, b2}), do: reduce({a1 * b2 + a2 * b1, b1 * b2})
+  def add({a1, b1}, {a2, b2}) do
+    reduce({a1 * b2 + a2 * b1, b1 * b2})
+  end
 
   @doc """
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract({a1, b1}, {a2, b2}), do: reduce({a1 * b2 - a2 * b1, b1 * b2})
+  def subtract({a1, b1}, {a2, b2}) do
+    reduce({a1 * b2 - a2 * b1, b1 * b2})
+  end
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply(a, b) do
+  def multiply({a1, b1}, {a2, b2}) do
+    reduce({a1 * a2, b1 * b2})
   end
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, den) do
+  def divide_by({a1, b1}, {a2, b2}) do
+    reduce({a1 * b2, a2 * b1})
   end
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs(a) do
+  def abs({n, d}) do
+    {do_abs(n), do_abs(d)}
   end
+
+  defp do_abs(num) when num < 0, do: -1 * num
+  defp do_abs(num), do: num
 
   @doc """
   Exponentiation of a rational number by an integer
@@ -52,6 +63,27 @@ defmodule RationalNumbers do
   Reduce a rational number to its lowest terms
   """
   @spec reduce(a :: rational) :: rational
-  def reduce({0, _b}), do: {0, 1}
-  def reduce(a), do: a
+  def reduce({n, d} = r) do
+    gcd =
+      r
+      |> abs()
+      |> find_gcd()
+
+    r = abs({div(n, gcd), div(d, gcd)})
+
+    cond do
+      n * d >= 0 ->
+        r
+
+      true ->
+        make_negative(r)
+    end
+  end
+
+  defp make_negative({n, d}), do: {n * -1, d}
+
+  def find_gcd({0, b}), do: b
+  def find_gcd({a, 0}), do: a
+  def find_gcd({a, b}) when b > a, do: find_gcd({b, a})
+  def find_gcd({a, b}), do: find_gcd({b, a - b})
 end
--- elixir/rational-numbers/test/rational_numbers_test.exs
+++ elixir/rational-numbers/test/rational_numbers_test.exs
@@ -70,7 +70,7 @@ defmodule RationalNumbersTest do
   end
 
   describe "Multiplication" do
-    @tag :pending
+    # @tag :pending
     test "Multiply two positive rational numbers" do
       r1 = {1, 2}
       r2 = {2, 3}
@@ -78,7 +78,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.multiply(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Multiply a negative rational number by a positive rational number" do
       r1 = {-1, 2}
       r2 = {2, 3}
@@ -86,7 +86,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.multiply(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Multiply two negative rational numbers" do
       r1 = {-1, 2}
       r2 = {-2, 3}
@@ -94,7 +94,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.multiply(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Multiply a rational number by its reciprocal" do
       r1 = {1, 2}
       r2 = {2, 1}
@@ -102,7 +102,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.multiply(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Multiply a rational number by 1" do
       r1 = {1, 2}
       r2 = {1, 1}
@@ -110,7 +110,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.multiply(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Multiply a rational number by 0" do
       r1 = {1, 2}
       r2 = {0, 1}
@@ -120,7 +120,7 @@ defmodule RationalNumbersTest do
   end
 
   describe "Division" do
-    @tag :pending
+    # @tag :pending
     test "Divide two positive rational numbers" do
       r1 = {1, 2}
       r2 = {2, 3}
@@ -128,7 +128,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.divide_by(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Divide a positive rational number by a negative rational number" do
       r1 = {1, 2}
       r2 = {-2, 3}
@@ -136,7 +136,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.divide_by(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Divide two negative rational numbers" do
       r1 = {-1, 2}
       r2 = {-2, 3}
@@ -144,7 +144,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.divide_by(r1, r2) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Divide a rational number by 1" do
       r1 = {1, 2}
       r2 = {1, 1}
@@ -154,35 +154,35 @@ defmodule RationalNumbersTest do
   end
 
   describe "Absolute value" do
-    @tag :pending
+    # @tag :pending
     test "Absolute value of a positive rational number" do
       r = {1, 2}
       result = {1, 2}
       assert RationalNumbers.abs(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Absolute value of a positive rational number with negative numerator and denominator" do
       r = {-1, -2}
       result = {1, 2}
       assert RationalNumbers.abs(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Absolute value of a negative rational number" do
       r = {-1, 2}
       result = {1, 2}
       assert RationalNumbers.abs(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Absolute value of a negative rational number with negative denominator" do
       r = {1, -2}
       result = {1, 2}
       assert RationalNumbers.abs(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Absolute value of zero" do
       r = {0, 1}
       result = {0, 1}
@@ -267,49 +267,49 @@ defmodule RationalNumbersTest do
   end
 
   describe "Reduction to lowest terms" do
-    @tag :pending
+    # @tag :pending
     test "Reduce a positive rational number to lowest terms" do
       r = {2, 4}
       result = {1, 2}
       assert RationalNumbers.reduce(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Reduce places the minus sign on the numerator" do
       r = {3, -4}
       result = {-3, 4}
       assert RationalNumbers.reduce(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Reduce a negative rational number to lowest terms" do
       r = {-4, 6}
       result = {-2, 3}
       assert RationalNumbers.reduce(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Reduce a rational number with a negative denominator to lowest terms" do
       r = {3, -9}
       result = {-1, 3}
       assert RationalNumbers.reduce(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Reduce zero to lowest terms" do
       r = {0, 6}
       result = {0, 1}
       assert RationalNumbers.reduce(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Reduce an integer to lowest terms" do
       r = {-14, 7}
       result = {-2, 1}
       assert RationalNumbers.reduce(r) == result
     end
 
-    @tag :pending
+    # @tag :pending
     test "Reduce one to lowest terms" do
       r = {13, 13}
       result = {1, 1}
