9cb26385c7961a693a8cec4bacc5a3088570338e
Colin Chauche <colin.chauche@gmail.com>
2021-12-22 12:51:50 -0600

    Improve performance of ListOps.concat/1

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -8,20 +8,14 @@ defmodule ListOps do
   @spec count(list) :: non_neg_integer
   def count([]), do: 0
 
-  def count([_h | t]) do
-    1 + count(t)
-  end
+  def count([_h | t]), do: 1 + count(t)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-    do_reverse(l, [])
-  end
+  def reverse(l) do: do_reverse(l, [])
 
   defp do_reverse([], reversed), do: reversed
 
-  defp do_reverse([h | t], reversed) do
-    do_reverse(t, [h | reversed])
-  end
+  defp do_reverse([h | t], reversed), do: do_reverse(t, [h | reversed])
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
@@ -39,7 +33,7 @@ defmodule ListOps do
 
   @type acc :: any
   @spec foldl(list, acc, (any, acc -> acc)) :: acc
-  def foldl([], acc, f), do: acc
+  def foldl([], acc, _f), do: acc
 
   def foldl([h | t], acc, f) do
     acc = f.(h, acc)
@@ -56,13 +50,9 @@ defmodule ListOps do
   @spec append(list, list) :: list
   def append([], b), do: b
   def append(a, []), do: a
-
-  def append([h | t], b) do
-    [h | append(t, b)]
-  end
+  def append([h | t], b), do: [h | append(t, b)]
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-    foldl(ll, [], &append(&2, &1))
-  end
+  def concat([]), do: []
+  def concat([h | t]), do: append(h, concat(t))
 end
