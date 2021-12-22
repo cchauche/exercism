9e7b6e58e87462c55ff3603890bd257fe06f95e4
Colin Chauche <colin.chauche@gmail.com>
2021-12-22 12:41:31 -0600

    Solve list-ops prompt with concat huge list small list timeout

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -25,26 +25,44 @@ defmodule ListOps do
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
+    l
+    |> foldl([], fn el, acc -> [f.(el) | acc] end)
+    |> reverse()
   end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
   def filter(l, f) do
+    l
+    |> foldl([], fn el, acc -> if f.(el), do: [el | acc], else: acc end)
+    |> reverse()
   end
 
   @type acc :: any
   @spec foldl(list, acc, (any, acc -> acc)) :: acc
-  def foldl(l, acc, f) do
+  def foldl([], acc, f), do: acc
+
+  def foldl([h | t], acc, f) do
+    acc = f.(h, acc)
+    foldl(t, acc, f)
   end
 
   @spec foldr(list, acc, (any, acc -> acc)) :: acc
   def foldr(l, acc, f) do
+    l
+    |> reverse()
+    |> foldl(acc, f)
   end
 
   @spec append(list, list) :: list
-  def append(a, b) do
+  def append([], b), do: b
+  def append(a, []), do: a
+
+  def append([h | t], b) do
+    [h | append(t, b)]
   end
 
   @spec concat([[any]]) :: [any]
   def concat(ll) do
+    foldl(ll, [], &append(&2, &1))
   end
 end
