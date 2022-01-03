99efa61994cb3650a8cbb43c98f0a27fdfba1129
Colin Chauche <colin.chauche@gmail.com>
2022-01-03 12:13:35 -0500

    Complete basketball-website prompt

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,15 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    keys = String.split(path, ".")
+    do_extract_from_path(data, keys)
   end
 
+  defp do_extract_from_path(nil, _keys), do: nil
+  defp do_extract_from_path(data, []), do: data
+  defp do_extract_from_path(data, [key | keys]), do: do_extract_from_path(data[key], keys)
+
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    keys = String.split(path, ".")
+    get_in(data, keys)
   end
 end
