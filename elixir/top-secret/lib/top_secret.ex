4cace1489bdc363f08f356aae9c9fb8f77f1c2f7
Colin Chauche <colin.chauche@gmail.com>
2022-02-11 12:03:40 -0500

    Complete top secret

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -16,9 +16,7 @@ defmodule TopSecret do
           fun
       end
 
-    arity =
-      fun
-      |> get_fun_arity()
+    arity = get_fun_arity(fun)
 
     name =
       fun
@@ -36,25 +34,36 @@ defmodule TopSecret do
   def decode_secret_message(string) do
     ast = to_ast(string)
 
-    ast_list =
-      case ast do
-        {:defmodule, _, args} ->
-          args
-          |> elem(1)
-          |> Keyword.get(:do)
+    do_decode_secret_message(ast, [])
+    |> assemble_message()
+  end
 
-        {:__block__, _, args} ->
-          args
-      end
+  defp do_decode_secret_message({:__block__, _data, ast_list}, acc) do
+    do_decode_secret_message(ast_list, acc)
+  end
 
-    do_decode_secret_message(ast_list, [])
+  defp do_decode_secret_message({:defmodule, _data, args}, acc) do
+    args
+    |> Enum.fetch!(1)
+    |> Keyword.get(:do)
+    |> do_decode_secret_message(acc)
   end
 
   defp do_decode_secret_message([], acc), do: acc
 
-  defp do_decode_secret_message([arg | args], acc) do
-    {_, acc} = decode_secret_message_part(arg, acc)
-    do_decode_secret_message(args, acc)
+  defp do_decode_secret_message([{:defmodule, _, _} = ast | ast_list], acc) do
+    acc = do_decode_secret_message(ast, acc)
+    do_decode_secret_message(ast_list, acc)
+  end
+
+  defp do_decode_secret_message([ast | ast_list], acc) do
+    {_, acc} = decode_secret_message_part(ast, acc)
+    do_decode_secret_message(ast_list, acc)
+  end
+
+  defp do_decode_secret_message(ast, acc) do
+    {_, acc} = decode_secret_message_part(ast, acc)
+    acc
   end
 
   defp get_fun_arity({_fun, _data, nil}), do: 0
@@ -65,50 +74,10 @@ defmodule TopSecret do
   defp trim_to_length(string, len) do
     String.slice(string, 0..(len - 1))
   end
-end
 
-# {:defmodule, [line: 1],
-#  [
-#    {:__aliases__, [line: 1], [:MyCalendar]},
-#    [
-#      do:
-#        {:__block__, [],
-#         [
-#           {:def, [line: 2],
-#            [
-#              {:busy?, [line: 2], [{:date, [line: 2], nil}, {:time, [line: 2], nil}]},
-#              [
-#                do:
-#                  {:and, [line: 3],
-#                   [
-#                     {:!=, [line: 3],
-#                      [
-#                        {{:., [line: 3], [{:__aliases__, [line: 3], [:Date]}, :day_of_week]},
-#                         [line: 3], [{:date, [line: 3], nil}]},
-#                        7
-#                      ]},
-#                     {:in, [line: 4],
-#                      [
-#                        {{:., [line: 4], [{:time, [line: 4], nil}, :hour]},
-#                         [no_parens: true, line: 4], []},
-#                        {:.., [line: 4], [10, 16]}
-#                      ]}
-#                   ]}
-#              ]
-#            ]},
-#           {:def, [line: 7],
-#            [
-#              {:yesterday?, [line: 7], [{:date, [line: 7], nil}]},
-#              [
-#                do:
-#                  {{:., [line: 8], [{:__aliases__, [line: 8], [:Date]}, :diff]}, [line: 8],
-#                   [
-#                     {{:., [line: 8], [{:__aliases__, [line: 8], [:Date]}, :utc_today]},
-#                      [no_parens: true, line: 8], []},
-#                     {:date, [line: 8], nil}
-#                   ]}
-#              ]
-#            ]}
-#         ]}
-#    ]
-#  ]}
+  defp assemble_message(msg_parts) do
+    msg_parts
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+end
