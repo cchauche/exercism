4462de06d643060c7c7f52950f7ec49da9fa7fc1
Colin Chauche <colin.chauche@gmail.com>
2022-02-22 09:05:41 -0500

    Simplify chessboard implementation

--- elixir/chessboard/lib/chessboard.ex
+++ elixir/chessboard/lib/chessboard.ex
@@ -1,17 +1,13 @@
 defmodule Chessboard do
-  def rank_range do
-    1..8
-  end
+  def rank_range, do: 1..8
 
-  def file_range do
-    ?A..?H
-  end
+  def file_range, do: ?A..?H
 
   def ranks do
     for i <- rank_range(), do: i
   end
 
   def files do
-    for i <- file_range(), do: to_string([i])
+    for i <- file_range(), do: <<i>>
   end
 end
