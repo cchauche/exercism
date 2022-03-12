22fcbaea60a2eb06614fcd863199a7ad10d17408
Colin Chauche <colin.chauche@gmail.com>
2022-03-11 21:28:50 -0500

    Pass all tests for lucas-numbers without Streams

---
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -0,0 +1,18 @@
+defmodule LucasNumbers do
+  @moduledoc """
+  Lucas numbers are an infinite sequence of numbers which build progressively
+  which hold a strong correlation to the golden ratio (φ or ϕ)
+
+  E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
+  """
+  @spec generate(integer()) :: [integer()]
+  def generate(1), do: [2]
+  def generate(2), do: [2, 1]
+
+  def generate(count) when is_integer(count) and count >= 1 do
+    [a, b | _] = nums = generate(count - 1) |> Enum.reverse()
+    [a + b | nums] |> Enum.reverse()
+  end
+
+  def generate(_), do: raise(ArgumentError, "count must be specified as an integer >= 1")
+end
