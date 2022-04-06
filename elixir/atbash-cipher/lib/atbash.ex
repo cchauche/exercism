c117ca9a4c5d0bb2b0087b8c5e735ec31e7ff12b
Colin Chauche <colin.chauche@gmail.com>
2022-04-06 10:58:03 -0400

    Complete atbash-cipher

--- elixir/atbash-cipher/lib/atbash.ex
+++ elixir/atbash-cipher/lib/atbash.ex
@@ -1,4 +1,12 @@
 defmodule Atbash do
+  @alphabet "abcdefghijklmnopqrstuvwxyz" |> String.split("", trim: true)
+  @reverse Enum.reverse(@alphabet)
+  @encode_map Enum.zip_reduce(@alphabet, @reverse, %{}, fn char1, char2, acc ->
+                Map.put_new(acc, char1, char2)
+              end)
+  @decode_map Enum.zip_reduce(@reverse, @alphabet, %{}, fn char1, char2, acc ->
+                Map.put_new(acc, char1, char2)
+              end)
   @doc """
   Encode a given plaintext to the corresponding ciphertext
 
@@ -9,9 +17,22 @@ defmodule Atbash do
   """
   @spec encode(String.t()) :: String.t()
   def encode(plaintext) do
+    plaintext
+    |> String.replace(~r/\s|\W/, "")
+    |> String.downcase()
+    |> String.split("", trim: true)
+    |> Enum.map(&(@encode_map[&1] || &1))
+    |> Enum.chunk_every(5)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.join(" ")
   end
 
   @spec decode(String.t()) :: String.t()
   def decode(cipher) do
+    cipher
+    |> String.replace(" ", "")
+    |> String.split("", trim: true)
+    |> Enum.map(&(@decode_map[&1] || &1))
+    |> Enum.join()
   end
 end
