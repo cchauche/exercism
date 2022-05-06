2e49e29eefa58ad4fd2934ae117b89161de89733
Colin Chauche <colin.chauche@gmail.com>
2022-05-06 11:43:42 -0400

    Refactor beer song to use better funciton arities for matches

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -4,24 +4,24 @@ defmodule BeerSong do
   """
   @spec verse(integer) :: String.t()
 
-  def verse(number) when number == 0 do
+  def verse(0) do
     """
     No more bottles of beer on the wall, no more bottles of beer.
     Go to the store and buy some more, 99 bottles of beer on the wall.
     """
   end
 
-  def verse(number) when number == 1 do
+  def verse(1) do
     """
-    #{number} bottle of beer on the wall, #{number} bottle of beer.
+    1 bottle of beer on the wall, 1 bottle of beer.
     Take it down and pass it around, no more bottles of beer on the wall.
     """
   end
 
-  def verse(number) when number == 2 do
+  def verse(2) do
     """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottle of beer on the wall.
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
     """
   end
 
