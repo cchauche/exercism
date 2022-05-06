5fd78950cef385c1df8d2c50c248b350fb2f2832
Colin Chauche <colin.chauche@gmail.com>
2022-05-06 11:43:24 -0400

    Complete dancing dots elixir prompt

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -10,6 +10,7 @@ defmodule DancingDots.Animation do
   defmacro __using__(_) do
     quote do
       alias DancingDots.Animation
+      alias DancingDots.Dot
       @behaviour Animation
       def init(opts), do: {:ok, opts}
 
@@ -22,13 +23,30 @@ defmodule DancingDots.Flicker do
   use DancingDots.Animation
 
   @impl Animation
-  def handle_frame(dot, frame_number, _opts) when rem(frame_number, 4) == 0 do
-    %{dot | opacity: dot.opacity / 2}
+  def handle_frame(%Dot{} = dot, frame_number, _opts) when rem(frame_number, 4) == 0 do
+    %Dot{dot | opacity: dot.opacity / 2}
   end
 
   def handle_frame(dot, _frame_number, _opts), do: dot
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl Animation
+  def init(opts) do
+    case opts[:velocity] do
+      velocity when is_integer(velocity) ->
+        {:ok, opts}
+
+      value ->
+        {:error,
+         "The :velocity option is required, and its value must be a number. Got: #{inspect(value)}"}
+    end
+  end
+
+  @impl Animation
+  def handle_frame(%Dot{} = dot, frame_number, opts) do
+    %Dot{dot | radius: dot.radius + (frame_number - 1) * opts[:velocity]}
+  end
 end
