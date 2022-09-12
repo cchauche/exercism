3c1ce3b391802b9829c253a32b873e72b0b5597b
Colin Chauche <colin.chauche@gmail.com>
2022-09-12 10:12:13 -0500

    Complete simple-calculator prompt ruby

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,9 +1,15 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
+  class UnsupportedOperation < StandardError
+  end
+
   def self.calculate(first_operand, second_operand, operation)
-    if not operation in ALLOWED_OPERATIONS
-      raise UnsuportedOperation
+    if not ALLOWED_OPERATIONS.include? operation
+      raise UnsupportedOperation
+    end
+    if (not first_operand.is_a? Numeric) || (not second_operand.is_a? Numeric)
+      raise ArgumentError
     end
     answer = 0
     begin
@@ -11,12 +17,16 @@ class SimpleCalculator
       when "+"
         answer = first_operand + second_operand
       when "/"
-        answer = first_operand / second_operand
+        begin
+          answer = first_operand / second_operand
+        rescue ZeroDivisionError => e
+          return "Division by zero is not allowed."
+        end
       when "*"
         answer = first_operand * second_operand
-      end 
+      end
     rescue
     end
-    "#{first_operand} #{operation} #{second_operand} = #{answer}"
+    return "#{first_operand} #{operation} #{second_operand} = #{answer}"
   end
 end
